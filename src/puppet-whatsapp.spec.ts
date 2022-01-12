263c5abb4b890cbc10d586a7e77e9546db2df222
shubuzuo <shubuzuo@gmail.com>
2022-01-12 16:14:02 +0800

    test PuppetWhatsapp invalid session restart no use session

--- src/puppet-whatsapp.spec.ts
+++ src/puppet-whatsapp.spec.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node --no-warnings --loader ts-node/esm
 
+import { firstValueFrom, fromEvent } from 'rxjs'
 import { test } from 'tstest'
+import { EventScanPayload, ScanStatus } from 'wechaty-puppet'
 
 import { PuppetWhatsapp } from './puppet-whatsapp.js'
 
@@ -27,4 +29,63 @@ test('PuppetWhatsapp perfect restart testing', async t => {
     console.error(e)
     t.fail(e as any)
   }
+})
+
+test('PuppetWhatsapp invalid session restart no use session', async t => {
+  test('PuppetWhatsapp start no use session', async t => {
+    const puppet = new PuppetWhatsAppTest()
+    await puppet.start(false)
+    const event$ = fromEvent(puppet, 'scan')
+
+    const future = firstValueFrom(event$)
+    const payload: EventScanPayload = {
+      status: ScanStatus.Unknown,
+    }
+    puppet.emit('scan', payload)
+
+    const result = await future
+
+    t.same(result, payload, 'should get scan payload')
+  })
+
+  test('PuppetWhatsapp start use legitimate session', async t => {
+    const puppet = new PuppetWhatsAppTest()
+    // use last generated session
+    await puppet.start(true)
+    const event$ = fromEvent(puppet, 'scan')
+
+    const future = firstValueFrom(event$)
+    const payload: EventScanPayload = {
+      status: ScanStatus.Unknown,
+    }
+    puppet.emit('scan', payload)
+
+    const result = await future
+
+    t.same(result, payload, 'should get scan payload')
+  })
+
+  test('PuppetWhatsapp start use illegal session', async t => {
+    const puppet = new PuppetWhatsAppTest()
+    const session = {
+      WABrowserId: 'invalid',
+      WASecretBundle: 'invalid',
+      WAToken1: 'invalid',
+      WAToken2: 'invalid'
+    }
+    await puppet.start(true, session)
+    const event$ = fromEvent(puppet, 'scan')
+
+    const future = firstValueFrom(event$)
+    const payload: EventScanPayload = {
+      status: ScanStatus.Unknown,
+    }
+    puppet.emit('scan', payload)
+
+    const result = await future
+
+    t.same(result, payload, 'should get scan payload')
+
+  })
+
 })
