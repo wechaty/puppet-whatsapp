8a1747f8029e76fe1173ccff0e12dcbbd36841d6
su-chang <su_chang@yeah.net>
2022-01-13 22:54:02 +0800

    feat: use gerror

--- src/pure-function-helpers/error-type.ts
+++ src/pure-function-helpers/error-type.ts
@@ -1,9 +1,19 @@
-import type { WAErrorType } from '../schema/error-type'
+import { WAErrorType, WXWORK_ERROR_TYPE } from '../schema/error-type.js'
+import { GError } from 'gerror'
 
-export default class WAError extends Error {
+export default class WAError {
 
-  constructor (type: WAErrorType, message: string) {
-    super(`${type} ${message}`)
+  constructor (
+    type: WAErrorType,
+    message: string,
+    details?: string,
+  ) {
+    GError.from({
+      code: type,
+      details,
+      message,
+      name: WXWORK_ERROR_TYPE[type],
+    })
   }
 
 }
