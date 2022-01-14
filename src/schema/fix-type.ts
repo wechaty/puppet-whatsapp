604e8f91b854b3c570983ab86c2edbc3b51e2b2e
shubuzuo <shubuzuo@gmail.com>
2022-01-14 11:45:02 +0800

    schema type adjust

--- src/schema/fix-type.ts
+++ src/schema/fix-type.ts
@@ -12,6 +12,29 @@ declare namespace WhatsAppRawFix {
     constructor(body: string, buttons: Array<{ body: string}>, title?: string | null, footer?: string | null)
 
   }
+  export interface Row {
+
+    title: string
+    description?: string
+  }
+  export interface Section {
+
+    title: string
+    rows: Row []
+  }
+
+  /** Message type List */
+  export class List {
+
+    body: string
+    buttonText: string
+    sections: Row []
+    title?: string | null
+    footer?: string | null
+    constructor(body: string, buttonText: string, sections: Array<any>, title?: string | null, footer?: string | null)
+
+  }
+
 }
 
 export default WhatsAppRawFix
