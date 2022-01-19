b79f27b4a4c65a454833ceed667974faf933a3c1
bung87 <crc32@qq.com>
2022-01-19 17:10:53 +0800

    fix pack

--- tests/thirdparty-vcard-parser.spec.ts
+++ tests/thirdparty-vcard-parser.spec.ts
@@ -1,16 +1,12 @@
 import { test } from 'tstest'
 import path from 'path'
 import fs from 'fs-extra'
-import { fileURLToPath } from 'url'
 import { parseVCards } from 'vcard4-ts'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 const TEST_FILENAME = 'testCard.vcf'
 
 test('vcard parser parse common data', async t => {
-  const filePath = path.join(__dirname, 'fixtures', TEST_FILENAME)
+  const filePath = path.join('tests', 'fixtures', TEST_FILENAME)
   const content = await fs.readFile(filePath, 'utf-8')
   const cards = parseVCards(content).vCards!
   const card = cards[0]
@@ -19,7 +15,7 @@ test('vcard parser parse common data', async t => {
 })
 
 test('vcard parser parse wa data', async t => {
-  const filePath = path.join(__dirname, 'fixtures', 'wacard.vcf')
+  const filePath = path.join('tests', 'fixtures', 'wacard.vcf')
   const content = await fs.readFile(filePath, 'utf-8')
   const cards = parseVCards(content).vCards!
   const card = cards[0]
