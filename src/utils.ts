fc0b839fb082eebd608b70f0d5a820c5252904e1
su-chang <su_chang@yeah.net>
2022-01-28 15:19:18 +0800

    fix: optimize invite link message to event

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+const InviteLinkRegex = /^(https?:\/\/)?chat\.whatsapp\.com\/(?:invite\/)?([a-zA-Z0-9_-]{22})$/
+
 export const sleep = async (milliseconds?: number) => {
   if (milliseconds) {
     await new Promise<void>(resolve => setTimeout(resolve, milliseconds))
@@ -10,6 +12,21 @@ export const isRoomId = (id: string) => {
 
 export const isContactId = (id: string) => {
   return /@c.us$/i.test(id)
+}
+
+export const isInviteLink = (link: string) => {
+  return InviteLinkRegex.test(link)
+}
+
+export const getInviteCode = (link: string) => {
+  const matched = link.match(InviteLinkRegex)
+  if (matched) {
+    if (matched.length === 3) {
+      const inviteCode = matched[2]
+      return inviteCode
+    }
+  }
+  return undefined
 }
 
 export const batchProcess = async (batchSize: number, list: any[], func: any) => {
