57a7223972f32c04bde945711787a9c0056bda79
bung87 <crc32@qq.com>
2022-02-08 21:17:58 +0800

    improve vcard parser test

--- tests/vcard-parser.spec.ts
+++ tests/vcard-parser.spec.ts
@@ -1,4 +1,6 @@
 import { test } from 'tstest'
+import { WA_ERROR_TYPE } from '../src/exceptions/error-type.js'
+import WAError from '../src/exceptions/whatsapp-error.js'
 import { parseVcard } from '../src/pure-function-helpers/vcard-parser.js'
 const contentContainOnePhone = 'BEGIN:VCARD\nVERSION:3.0\nN:康;龙;;;\nFN:康龙\nitem1.TEL;waid=8613240330438:+86 132 4033 0438\nitem1.X-ABLabel:手机\nEND:VCARD'
 
@@ -10,9 +12,7 @@ test('vcard parser for only one phone of contact', async t => {
 })
 
 test('vcard parser for multi phones of contact', async t => {
-  try {
-    parseVcard(contentContainMultiPhones)
-  } catch (error) {
-    t.ok((error as any).code === 2006)
-  }
+  const fn = () => parseVcard(contentContainMultiPhones)
+  const data =  [{ phone:'+86 24 6278 1276', waid:'862462781276@c.us' }, { phone:'+52 729739', waid:'52729739@c.us' }]
+  t.throws(fn, WAError,  new WAError(WA_ERROR_TYPE.ERR_MSG_CONTACT, `This card contains more than 1 phone number, detail: ${JSON.stringify(data)}`))
 })
