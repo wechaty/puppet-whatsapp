3c8355abc75ee3dda6b98f23af91e3961741ef13
suchang <su_chang@yeah.net>
2022-02-09 19:24:16 +0800

    fix: add test case for contact message parser

--- tests/vcard-parser.spec.ts
+++ tests/vcard-parser.spec.ts
@@ -2,12 +2,18 @@ import { test } from 'tstest'
 import { WA_ERROR_TYPE } from '../src/exceptions/error-type.js'
 import WAError from '../src/exceptions/whatsapp-error.js'
 import { parseVcard } from '../src/pure-function-helpers/vcard-parser.js'
-const contentContainOnePhone = 'BEGIN:VCARD\nVERSION:3.0\nN:康;龙;;;\nFN:康龙\nitem1.TEL;waid=8613240330438:+86 132 4033 0438\nitem1.X-ABLabel:手机\nEND:VCARD'
+const contentFromAndroid = 'BEGIN:VCARD\nVERSION:3.0\nN:康;龙;;;\nFN:康龙\nitem1.TEL;waid=8613240330438:+86 132 4033 0438\nitem1.X-ABLabel:手机\nEND:VCARD'
+const contentFromIOS = 'BEGIN:VCARD\nVERSION:3.0\nN:🐉;socialbear;;;\nFN:socialbear 🐉\nTEL;type=CELL;type=VOICE;waid=8613240330438:+86 132 4033 0438\nEND:VCARD'
 
 const contentContainMultiPhones = 'BEGIN:VCARD\nVERSION:3.0\nN:测试企业9Group;Chatie多人通话;;;\nFN:Chatie多人通话\nitem1.TEL:+86 24 6278 1276\nitem1.X-ABLabel:公费电话\nitem2.TEL:+52 729739\nitem2.X-ABLabel:公费电话\nEND:VCARD'
 
-test('vcard parser for only one phone of contact', async t => {
-  const card = parseVcard(contentContainOnePhone)
+test('vcard parser for only one phone of contact from ANDROID', async t => {
+  const card = parseVcard(contentFromAndroid)
+  t.ok(card.TEL![0]!.waid === '8613240330438@c.us')
+})
+
+test('vcard parser for only one phone of contact from IOS', async t => {
+  const card = parseVcard(contentFromIOS)
   t.ok(card.TEL![0]!.waid === '8613240330438@c.us')
 })
 
