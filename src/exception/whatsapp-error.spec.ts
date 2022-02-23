477193c6f158c0a79f18ca65f2f52a20a100164e
suchang <su_chang@yeah.net>
2022-02-23 16:03:33 +0800

    feat: add test for whatsapp error class

---
+++ src/exception/whatsapp-error.spec.ts
@@ -0,0 +1,23 @@
+#!/usr/bin/env node --no-warnings --loader ts-node/esm
+
+import { test } from 'tstest'
+import { WA_ERROR_TYPE } from './error-type.js'
+
+import WAError from './whatsapp-error.js'
+
+function throwError () {
+  throw WAError(WA_ERROR_TYPE.ERR_INIT, 'no cache manager')
+}
+test('WAError should throw', async t => {
+  t.throws(throwError, WAError, WAError(WA_ERROR_TYPE.ERR_INIT, 'no cache manager'))
+})
+
+test('WAError instanceof error', async t => {
+  const err = WAError(WA_ERROR_TYPE.ERR_INIT, 'no cache manager')
+  t.ok(err instanceof Error)
+})
+
+test('WAError should has message', async t => {
+  const err = WAError(WA_ERROR_TYPE.ERR_INIT, 'no cache manager')
+  t.ok(err.message === 'no cache manager')
+})
