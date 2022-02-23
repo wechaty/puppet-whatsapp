40e1255b3c1e8ef77078334698d4a2f575701dd0
suchang <su_chang@yeah.net>
2022-02-23 20:19:13 +0800

    feat: add whatsapp base class

--- src/whatsapp/whatsapp-base.ts
+++ src/whatsapp/whatsapp-base.ts
@@ -1,3 +1,47 @@
-export default class WhatsAppBase {
+/* eslint-disable no-case-declarations */
+/* eslint-disable import/no-duplicates */
+import { EventEmitter as EE } from 'ee-ts'
+import { WA_ERROR_TYPE } from '../exception/error-type.js'
+import WAError from '../exception/whatsapp-error.js'
+import type { ManagerEvents } from '../manager-event.js'
+import type Manager from '../manager.js'
+import type { WhatsAppClientType } from '../schema/whatsapp-type.js'
+
+export default class WhatsAppBase extends EE<ManagerEvents> {
+
+  protected botId?: string
+  protected pendingLogoutEmitTimer?: NodeJS.Timer
+  protected whatsAppClient?: WhatsAppClientType
+
+  constructor (protected manager: Manager) {
+    super()
+  }
+
+  public clearWhatsAppRelatedData () {
+    this.botId = undefined
+    this.whatsAppClient = undefined
+    this.clearPendingLogoutEmitTimer()
+  }
+
+  public getBotId () {
+    if (!this.botId) {
+      throw WAError(WA_ERROR_TYPE.ERR_INIT, 'This bot is not login')
+    }
+    return this.botId
+  }
+
+  public clearPendingLogoutEmitTimer () {
+    if (this.pendingLogoutEmitTimer) {
+      clearTimeout(this.pendingLogoutEmitTimer)
+      this.pendingLogoutEmitTimer = undefined
+    }
+  }
+
+  public getWhatsAppClient () {
+    if (!this.whatsAppClient) {
+      throw WAError(WA_ERROR_TYPE.ERR_INIT, 'Not init whatsapp')
+    }
+    return this.whatsAppClient
+  }
 
 }
