2742849eb7d0ceed70549c33221c226e9b16c1a9
NickWang <brotherstyx@gmail.com>
2022-02-23 19:40:15 +0800

    feat: 🎸 add cacheManager in manager

--- src/manager.ts
+++ src/manager.ts
@@ -1,3 +1,42 @@
+import { log } from './config.js'
+import { CacheManager } from './data/cache-manager.js'
+import { WA_ERROR_TYPE } from './exception/error-type.js'
+import WAError from './exception/whatsapp-error.js'
+
+const PRE = 'manager'
+
 export default class Manager {
+
+  cacheManager?: CacheManager
+
+  /**
+   * Cache Section
+   */
+
+  public async initCache (userId: string) {
+    log.info(PRE, `initCache(${userId})`)
+    if (this.cacheManager) {
+      log.warn(PRE, 'initCache() already initialized, skip the init...')
+      return
+    }
+    await CacheManager.init(userId)
+    this.cacheManager = CacheManager.Instance
+  }
+
+  public async releaseCache () {
+    log.info(PRE, 'releaseCache()')
+    if (this.cacheManager) {
+      log.warn(PRE, 'releaseCache() already initialized, skip the init...')
+      return
+    }
+    await CacheManager.release()
+  }
+
+  public async getCacheManager () {
+    if (!this.cacheManager) {
+      throw WAError(WA_ERROR_TYPE.ERR_INIT, 'no cache manager')
+    }
+    return this.cacheManager
+  }
 
 }
