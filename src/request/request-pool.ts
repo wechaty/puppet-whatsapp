261bc371a55d952c07a2d79d2d74582cac59c3f9
suchang <su_chang@yeah.net>
2022-02-23 17:28:13 +0800

    feat: add request pool

--- src/request/request-pool.ts
+++ src/request/request-pool.ts
@@ -1,3 +1,45 @@
-export default class RequestPool {
+import { WA_ERROR_TYPE } from '../exception/error-type.js'
+import WAError from '../exception/whatsapp-error.js'
+export class RequestPool {
+
+  private static _instance?: RequestPool
+  private poolMap: {[id: string]: Function[]} = {}
+  private constructor () {}
+
+  public static get Instance () {
+    if (!this._instance) {
+      this._instance = new RequestPool()
+    }
+    return this._instance
+  }
+
+  public pushRequest (id: string, timeout?: number) {
+    const callback = new Promise<void>((resolve, reject) => {
+      if (!this.poolMap[id]) {
+        this.poolMap[id] = []
+      }
+      this.poolMap[id]!.push(resolve)
+      if (timeout) {
+        setTimeout(reject, timeout)
+      }
+    }).catch(() => {
+      delete this.poolMap[id]
+      throw WAError(WA_ERROR_TYPE.ERR_REQUEST_TIMEOUT, `TIMEOUT when processing request :${id}`)
+    })
+    return callback
+  }
+
+  public resolveRequest (id: string) {
+    const callbacks = this.poolMap[id]
+    if (!callbacks || callbacks.length === 0) {
+      return
+    }
+    callbacks.forEach(cb => cb())
+    delete this.poolMap[id]
+  }
+
+  public clearPool () {
+    this.poolMap = {}
+  }
 
 }
