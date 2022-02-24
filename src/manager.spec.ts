e1ad1b094dbfdb3870e627cd2189b37ea075d63e
suchang <su_chang@yeah.net>
2022-02-24 15:35:24 +0800

    fix: optimize code

--- src/manager.spec.ts
+++ src/manager.spec.ts
@@ -7,24 +7,10 @@ function genTimestamp () {
   return Math.floor(Date.now() / 1000) - 2 * 24 * 3600 - Math.floor(Math.random() * 10000)
 }
 
-const fakeMsgListWithin3Days = [
-  { timestamp: genTimestamp() },
-  { timestamp: genTimestamp() },
-  { timestamp: genTimestamp() },
-  { timestamp: genTimestamp() },
-  { timestamp: genTimestamp() },
-  { timestamp: genTimestamp() },
-  { timestamp: genTimestamp() },
-  { timestamp: genTimestamp() },
-  { timestamp: genTimestamp() },
-  { timestamp: genTimestamp() },
-] as any
+const fakeMsgListWithin3Days = Array(10).fill({ timestamp: genTimestamp() }) as any
 
-const fakeMsgListWithin5Days = [
-  { timestamp: 1644823352 },
-  { timestamp: 1644823353 },
-  { timestamp: 1644823354 },
-  { timestamp: 1644823355 },
+const fakeMsgListOver3Days = [
+  Array(10).fill({ timestamp: 1644823352 }),
   ...fakeMsgListWithin3Days,
 ] as any
 
@@ -46,7 +32,7 @@ test('filterFetchedMessages within 5 days', async t => {
   const manager = new ManagerTest({})
   await manager.initCache(contactId)
   // @ts-ignore
-  const list = await manager.filterFetchedMessages(contactId, fakeMsgListWithin5Days)
+  const list = await manager.filterFetchedMessages(contactId, fakeMsgListOver3Days)
   t.ok(list.length === fakeMsgListWithin3Days.length)
   await manager.releaseCache()
 })
