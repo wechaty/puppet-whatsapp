523fe59aae5c1401d0ad2207a42dab3ef6a472ae
NickWang <brotherstyx@gmail.com>
2022-02-28 12:04:41 +0800

    fix: 🐛 remove unused comment

--- src/helper/pure-function/contact-raw-payload-parser.spec.ts
+++ src/helper/pure-function/contact-raw-payload-parser.spec.ts
@@ -1,7 +1,4 @@
 /* eslint-disable sort-keys */
-// 11: 30: 12 INFO MIXIN_CONTACT contactRawPayloadParser whatsAppPayload({ "id": { "server": "c.us", "user": "8618500946096", "_serialized": "8618500946096@c.us" }, "number": "8618500946096", "type": "in", "isMe": true, "isUser": true, "isGroup": false, "isWAContact": true, "isMyContact": false, "isBlocked": false, "avatar": "https://pps.whatsapp.net/v/t61.24694-24/263399770_379490933975443_817845888742244692_n.jpg?ccb=11-4&oh=9c9c04bf8b10ccfef07f5334a34845f7&oe=6221A5B8" }) result({ "avatar": "https://pps.whatsapp.net/v/t61.24694-24/263399770_379490933975443_817845888742244692_n.jpg?ccb=11-4&oh=9c9c04bf8b10ccfef07f5334a34845f7&oe=6221A5B8", "friend": false, "gender": 0, "id": "8618500946096@c.us", "name": "MyOnlyStarCN", "phone": ["8618500946096"], "type": 1, "weixin": "8618500946096" })
-
-// 11: 30: 12 INFO MIXIN_CONTACT contactRawPayloadParser whatsAppPayload({ "id": { "server": "c.us", "user": "918879481247", "_serialized": "918879481247@c.us" }, "number": "918879481247", "isBusiness": false, "isEnterprise": false, "labels": [], "pushname": "Prince Arora", "statusMute": false, "type": "in", "isMe": false, "isUser": true, "isGroup": false, "isWAContact": true, "isMyContact": false, "isBlocked": false }) result({ "friend": false, "gender": 0, "id": "918879481247@c.us", "name": "Prince Arora", "phone": ["918879481247"], "type": 1, "weixin": "918879481247" })
 
 import { test } from 'tstest'
 import { parserContactRawPayload } from './contact-raw-payload-parser.js'
